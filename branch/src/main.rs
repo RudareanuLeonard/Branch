41fdcd8ed6d4ce1541ec1875887bcdbab436d66e
RudareanuLeonard <leonardrudareanu@gmail.com>
2026-07-01 21:03:59 +0300

    basic user input

--- branch/src/main.rs
+++ branch/src/main.rs
@@ -3,6 +3,8 @@ mod state;
 mod input;
 mod filesystem;
 
+use std::io;
+
 use filesystem::scan_current_directory;
 // use state::get_files;
 // use ui::display_files;
@@ -11,8 +13,11 @@ use state::AppState;
 use ui::display_files;
 
 
+
 fn main() {
     println!("Hello, this is MAIN file!");
+
+    
 
     let files = scan_current_directory();
 
@@ -32,7 +37,32 @@ fn main() {
     println!("");
     println!("");
 
-    display_files(&appState);
+    loop{
+        display_files(&appState);
+        println!("");
+        println!("Enter a key");
+        let mut key: String = String::new();
+
+        io::stdin().read_line(&mut key).expect("Wrong input !");
+
+        key = key.trim().to_string();
+
+        if key == "w"{
+            println!("Up");
+        }
+        else if key == "s"{
+            println!("Down");
+        }
+        else if key == "q"{
+            break;
+        }
+        else{
+            println!("Key = {}", key);
+        }
+
+    }
+
+    
 
     // keyboard_read();
 
